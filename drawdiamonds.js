1162b3b3680c9a8fa551932d420f205847611c2c
huydau91 <dauthehuy91@gmail.com>
2017-05-06 19:40:04 +0700

    update

---
+++ drawdiamonds.js
@@ -0,0 +1,89 @@
+console.log("Nhập n:");
+
+var prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(['n'], function (err, result) {
+	var n = result.n;
+	var str = '';
+	var size = n*2;
+	var a = n-1;
+	for (var i = 0; i<size+1; i++){
+		if (i == 0 || i == size){
+			for (var j=0; j<size+1; j++){
+				if (j == n){
+					str += "*";
+				}
+				else
+					str += " ";
+			}
+			str += '\n';
+		}
+		else {
+			if (i <= n){
+				for (var j=0; j<size+1; j++){
+					if (j == n-i || j == n+i){
+						str += "*";
+					}
+					else
+						str += " ";
+				}
+				str += '\n';
+			}
+			else {
+				for (var j=0; j<size+1; j++){
+					if (j == n-a || j == n+a){
+						str += "*";
+					}
+					else
+						str += " ";
+				}
+				a--;
+				str += '\n';
+			}
+		}
+	}
+	console.log(str);
+});
+// function drawdiamonds(n){
+// 			var str = '';
+// 			var size = n*2;
+// 			var a = n-1;
+// 			for (var i = 0; i<size+1; i++){
+// 				if (i == 0 || i == size){
+// 					for (var j=0; j<size+1; j++){
+// 						if (j == n){
+// 							str += "*";
+// 						}
+// 						else
+// 							str += " ";
+// 					}
+// 					str += '\n';
+// 				}
+// 				else {
+// 					if (i <= n){
+// 						for (var j=0; j<size+1; j++){
+// 							if (j == n-i || j == n+i){
+// 								str += "*";
+// 							}
+// 							else
+// 								str += " ";
+// 						}
+// 						str += '\n';
+// 					}
+// 					else {
+// 						for (var j=0; j<size+1; j++){
+// 							if (j == n-a || j == n+a){
+// 								str += "*";
+// 							}
+// 							else
+// 								str += " ";
+// 						}
+// 						a--;
+// 						str += '\n';
+// 					}
+// 				}
+// 			}
+// 			console.log(str);
+// 		}
--- drawdiamonds/drawdiamonds.js
+++ drawdiamonds/drawdiamonds.js
@@ -1,43 +1,97 @@
 function drawdiamonds(n){
-			var str = '';
-			var size = n*2;
-			var a = n-1;
-			for (var i = 0; i<size+1; i++){
-				if (i == 0 || i == size){
-					for (var j=0; j<size+1; j++){
-						if (j == n){
-							str += "*";
-						}
-						else
-							str += " ";
-					}
-					str += '\n';
+	var str = '';
+	var size = n*2;
+	var a = n-1;
+	for (var i = 0; i<size+1; i++){
+		if (i == 0 || i == size){
+			for (var j=0; j<size+1; j++){
+				if (j == n){
+					str += "*";
 				}
-				else {
-					if (i <= n){
-						for (var j=0; j<size+1; j++){
-							if (j == n-i || j == n+i){
-								str += "*";
-							}
-							else
-								str += " ";
-						}
-						str += '\n';
-					}
-					else {
-						for (var j=0; j<size+1; j++){
-							if (j == n-a || j == n+a){
-								str += "*";
-							}
-							else
-								str += " ";
-						}
-						a--;
-						str += '\n';
-					}
-				}
+				else
+					str += " ";
 			}
-			console.log(str);
+			str += '\n';
 		}
+		else {
+			if (i <= n){
+				for (var j=0; j<size+1; j++){
+					if (j == n-i || j == n+i){
+						str += "*";
+					}
+					else
+						str += " ";
+				}
+				str += '\n';
+			}
+			else {
+				for (var j=0; j<size+1; j++){
+					if (j == n-a || j == n+a){
+						str += "*";
+					}
+					else
+						str += " ";
+				}
+				a--;
+				str += '\n';
+			}
+		}
+	}
+	console.log(str);
+}
 
-drawdiamonds(7)
+drawdiamonds(9)  
+
+
+  // console.log("Nhap n:");
+
+  // var prompt = require('prompt');
+
+  // prompt.start();
+
+  // prompt.get('n', function (err, result) {
+  // 	var n = result.n;
+  // 	var str = '';
+  // 	var size = n*2;
+  // 	var a = n-1;
+  // 	for (var i = 0; i < size + 1; i++){
+  // 		if (i == 0 || i == size){
+  // 			for (var j=0; j<size+1; j++){
+  // 				if (j == n){
+  // 					str += "*";
+  // 				}
+  // 				else{
+  // 					str += " ";
+  // 				}
+  // 			}
+  // 			str += '\n';
+  // 		}
+  // 		else {
+  // 			if (i <= n){
+  // 				for (var j=0; j<size+1; j++){
+  // 					if (j == n-i || j == n+i){
+  // 						str += "*";
+  // 					}
+  // 					else{
+  // 						str += " ";
+  // 					}
+  // 				}
+  // 				str += '\n';
+  // 			}
+  // 			else {
+  // 				for (var j=0; j<size+1; j++){
+  // 					if (j == n-a || j == n+a){
+  // 						str += "*";
+  // 					}
+  // 					else{
+  // 						str += " ";
+  // 					}
+  // 				}
+  // 				a--;
+  // 				str += '\n';
+  // 			}
+  // 		}
+  // 	}
+  // 	console.log(str);
+  // });
+
